98612496e84dd52eb4a020f378c8e248f15a155d
Chris Cowell <christopher.w.cowell@gmail.com>
2018-10-01 23:01:40 -0700

    store historical data in pandas dataframe
    Plus more refactoring in the direction of better OO design

--- src/HaircutsAndOatmeal.py
+++ src/HaircutsAndOatmeal.py
@@ -8,44 +8,16 @@ def clean_tickers(tickers: list) -> list:
     return tickers
 
 
-# --- CONSTANTS ---
-
 buy_budget = 1000.0  # how much we spend on any stock when buying it
-
 rise_limit = 0.1  # how high a stock can rise before we sell it, to take profits
 sink_limit = 0.05  # how far a stock can sink before we sell it, to protect against further losses
 cool_off_span = 3  # how long to wait after selling a stock, before automatically buying that stock again
-
-# def load_single_ticker_price_history(ticker):
-#     path = 'datasets/SandP500/individual_stocks_5yr/{}_data.csv'.format(ticker)
-#     file = open(path, 'r')
-#     file.readline()  # throw away the header row
-#     prices = []
-#     for line in file:
-#         line_parts = line.split(',')
-#         closing_price = float(line_parts[4])
-#         prices.append(closing_price)
-#     return prices
-#
-#
-# price_histories = dict()
-# for s_and_p_ticker in s_and_p_tickers:
-#     price_histories[s_and_p_ticker] = load_single_ticker_price_history(s_and_p_ticker)
-
-
-# # --- CREATE STOCKS ---
-# abt_stock = Models.Stock('ABT', price_histories['ABT'])
-# mmm_stock = Models.Stock('MMM', price_histories['MMM'])
-# stocks = [abt_stock, mmm_stock]
-
-
-# --- CREATE PORTFOLIO ---
 
 # open DB
 conn = sqlite3.connect('../data/SandP500.sqlite3')
 cursor = conn.cursor()
 
-# get ticker symbols
+# get list of all ticker symbols
 sql = 'SELECT DISTINCT ticker FROM historical_prices;'
 cursor.execute(sql)
 tickers = []
@@ -61,18 +33,11 @@ tickers = clean_tickers(tickers)
 
 # add each stock to portfolio
 initial_cash_balance = buy_budget * len(tickers)
-portfolio = Models.Portfolio(initial_cash_balance)
-for ticker in tickers:
+portfolio = Models.Portfolio(initial_cash_balance, buy_budget, rise_limit, sink_limit, cool_off_span)
+
+for ticker in tickers[:3]:
     portfolio.add_ticker(ticker)
 
-
-# --- MAIN LOGIC ---
-
-print("initial cash balance: ${:.2f}\n".format(portfolio.cash_balance))
-
-grand_profit = portfolio.cash_balance - initial_cash_balance
-grand_percent_change = grand_profit / initial_cash_balance * 100
-
-print("\nfinal cash balance: ${:.0f}".format(portfolio.cash_balance))
-print("final amount change:: ${:.0f}".format(grand_profit))
-print("final percent change: {:.0f}%".format(grand_percent_change))
+portfolio.print_initial_stats()
+portfolio.run_simulation()
+portfolio.print_final_stats()
--- src/Models.py
+++ src/Models.py
@@ -7,28 +7,24 @@ class Stock:
     ticker = None
     shares = 0.0
     price_history = None
-    bought_at_price = 0.0
-    last_sell_date = 0
+    last_bought_at_price = 0.0
+    last_sell_date_idx = 0
 
     def __init__(self, ticker, price_history):
         self.ticker = ticker
         self.price_history = price_history
 
-    def __str__(self):
-        return "ticker: {}, shares: {}, bought_at_price: {}".format(
-            self.ticker, self.shares, self.bought_at_price)
-
-    def buy(self, date, cash_balance, buy_budget):
-        bought_shares = buy_budget / self.price_history[date]
+    def buy(self, date_idx: int, cash_balance: float, buy_budget: float) -> float:
+        bought_shares = buy_budget / self.price_history.iat[date_idx, 1]
         print("{}:  buy {:.2f} shares at {:.2f} for ${:.2f} on date {}".format(
-            self.ticker, bought_shares, self.price_history[date], buy_budget, date))
+            self.ticker, bought_shares, self.price_history.iat[date_idx, 1], buy_budget, date_idx))
         self.shares = bought_shares
         cash_balance -= buy_budget
-        self.bought_at_price = self.price_history[date]
+        self.last_bought_at_price = self.price_history.iat[date_idx, 1]
         return cash_balance
 
-    def sell(self, date, cash_balance, original_purchase_value):
-        sell_value = self.shares * self.price_history[date]
+    def sell(self, date_idx: int, cash_balance: float, original_purchase_value: float) -> float:
+        sell_value = self.shares * self.price_history.iat[date_idx, 1]
         profit_or_loss = sell_value - original_purchase_value
 
         if profit_or_loss >= 0:
@@ -37,25 +33,25 @@ class Stock:
             text_color = 'red'
 
         cprint("{}: ${:.2f}: sell {:.2f} shares at {:.2f} for ${:.2f} on date {}".format(
-            self.ticker, profit_or_loss, self.shares, self.price_history[date], sell_value, date),
+            self.ticker, profit_or_loss, self.shares, self.price_history.iat[date_idx, 1], sell_value, date_idx),
             text_color)
         cash_balance += sell_value
         self.shares = 0
-        self.last_sell_date = date
+        self.last_sell_date_idx = date_idx
         return cash_balance
 
-    def is_below_sink_limit(self, date, sink_limit_percent):
-        sink_limit_price = self.bought_at_price - (self.bought_at_price * sink_limit_percent)
-        return self.price_history[date] <= sink_limit_price
+    def is_below_sink_limit(self, date_idx: int, sink_limit_percent: float) -> bool:
+        sink_limit_price = self.last_bought_at_price - (self.last_bought_at_price * sink_limit_percent)
+        return self.price_history.iat[date_idx, 1] <= sink_limit_price
 
-    def is_above_rise_limit(self, date, rise_limit_percent):
-        rise_limit_price = self.bought_at_price + (self.bought_at_price * rise_limit_percent)
-        return self.price_history[date] >= rise_limit_price
+    def is_above_rise_limit(self, date_idx: int, rise_limit_percent: float) -> bool:
+        rise_limit_price = self.last_bought_at_price + (self.last_bought_at_price * rise_limit_percent)
+        return self.price_history.iat[date_idx, 1] >= rise_limit_price
 
-    def are_any_shares_owned(self):
+    def are_any_shares_owned(self) -> bool:
         return self.shares > 0
 
-    def are_no_shares_owned(self):
+    def are_no_shares_owned(self) -> bool:
         return not self.are_any_shares_owned()
 
 
@@ -63,56 +59,71 @@ class Portfolio:
     stocks = []
     cash_balance = 0.0
     initial_cash_balance = 0.0
+    buy_budget = 0.0
+    rise_limit = 0.0
+    sink_limit = 0.0
+    cool_off_span = 3  # measured in days
 
-    def __init__(self, initial_cash_balance):
+    def __init__(self, initial_cash_balance, buy_budget, rise_limit, sink_limit, cool_off_span):
         self.initial_cash_balance = initial_cash_balance
         self.cash_balance = initial_cash_balance
+        self.buy_budget = buy_budget
+        self.rise_limit = rise_limit
+        self.sink_limit = sink_limit
+        self.cool_off_span = cool_off_span
 
     def add_ticker(self, ticker: str) -> None:
         # open DB
         conn = sqlite3.connect('../data/SandP500.sqlite3')
         cursor = conn.cursor()
 
-        # get price history into pandas dataframe
-        sql = f"SELECT (ticker, date, closing_price) FROM historical_prices WHERE ticker = '{ticker}' ORDER BY date;"
-        dataframe = pandas.read_sql(sql, conn)
-
-
-        # cursor.execute(sql)
-        ticker_prices = []
-        # for row in cursor:
-        #     ticker_price = dict()
-        #     ticker_price['ticker']
-        #     ticker_prices.append(row)
+        # load price history from DB into a pandas dataframe
+        sql = f"SELECT date, closing_price FROM historical_prices WHERE ticker = '{ticker}' ORDER BY date;"
+        price_history = pandas.read_sql(sql, conn)
 
         # close DB
         cursor.close()
-        conn.commit()
         conn.close()
-        stock = Stock(ticker)
 
+        # create a new Stock instance and add it to the portfolio
+        new_stock = Stock(ticker, price_history)
+        self.stocks.append(new_stock)
+        print(f"added {ticker} to portfolio")
 
     def ramp_up(self) -> None:
-        """ buy initial shares of each stock """
+        """Buy initial shares of each stock."""
         for stock in self.stocks:
             initial_date_idx = 0
-            stock.buy(initial_date_idx, self.cash_balance, buy_budget)
+            stock.buy(initial_date_idx, self.cash_balance, self.buy_budget)
 
     def run_simulation(self) -> None:
-        """ actually run the time machine """
+        """Run the time machine. This is where the magic happens!"""
         for date_idx in range(1, len(self.stocks[0].price_history)):
             for stock in self.stocks:
-                time_to_take_gains = stock.is_above_rise_limit(date_idx, rise_limit)
-                time_to_limit_losses = stock.is_below_sink_limit(date_idx, sink_limit)
-                cool_off_expired = date_idx - stock.last_sell_date >= cool_off_span
+                time_to_take_gains = stock.is_above_rise_limit(date_idx, self.rise_limit)
+                time_to_limit_losses = stock.is_below_sink_limit(date_idx, self.sink_limit)
+                cool_off_expired = date_idx - stock.last_sell_date_idx >= self.cool_off_span
 
                 if stock.are_any_shares_owned() and (time_to_take_gains or time_to_limit_losses):
-                    self.cash_balance = stock.sell(date_idx, self.cash_balance, buy_budget)
+                    self.cash_balance = stock.sell(date_idx, self.cash_balance, self.buy_budget)
                 elif stock.are_no_shares_owned() and cool_off_expired:
-                    self.cash_balance = stock.buy(date_idx, self.cash_balance, buy_budget)
+                    self.cash_balance = stock.buy(date_idx, self.cash_balance, self.buy_budget)
 
     def ramp_down(self) -> None:
-        """ sell all shares of all stocks at the end of the simulation """
+        """Sell all shares of all stocks at the end of the simulation."""
         for stock in self.stocks:
             if stock.are_any_shares_owned():
-                self.cash_balance = stock.sell(9, self.cash_balance, buy_budget)
+                self.cash_balance = stock.sell(9, self.cash_balance, self.buy_budget)
+
+    def print_initial_stats(self) -> None:
+        """Print info about the portfolio before any trades happen."""
+        print("initial cash balance: ${:.2f}\n".format(self.initial_cash_balance))
+
+    def print_final_stats(self) -> None:
+        grand_profit = self.cash_balance - self.initial_cash_balance
+        grand_percent_change = grand_profit / self.initial_cash_balance * 100
+
+        print()
+        print("final cash balance: ${:.0f}".format(self.cash_balance))
+        print("final amount change:: ${:.0f}".format(grand_profit))
+        print("final percent change: {:.0f}%".format(grand_percent_change))
